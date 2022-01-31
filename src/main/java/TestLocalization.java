90c419077eabee3d78f2ec4b71430d782ada17f1
-Denux <timon.klinkert2005@gmail.com>
2022-01-31 20:55:35 +0100

    Deleted test-class

--- src/main/java/TestLocalization.java
+++
@@ -1,9 +0,0 @@
-import de.lightbolt.meeting.utils.localization.Language;
-import de.lightbolt.meeting.utils.localization.LocalizationUtils;
-
-public class TestLocalization {
-
-    public static void main(String[] args) {
-        LocalizationUtils.getLocale(Language.DE);
-    }
-}
