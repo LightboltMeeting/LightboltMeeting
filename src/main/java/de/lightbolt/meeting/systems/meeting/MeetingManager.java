f0e7bbee23b791ea184bf35b00639ed2f0d27dbc
Dynxsty <jasonlessenich@gmail.com>
2022-02-02 17:28:19 +0100

    fixed Permission Overrides

--- src/main/java/de/lightbolt/meeting/systems/meeting/MeetingManager.java
+++ src/main/java/de/lightbolt/meeting/systems/meeting/MeetingManager.java
@@ -9,6 +9,8 @@ import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.*;
 
+import java.util.Collections;
+
 /**
  * Schedules and handles {@link de.lightbolt.meeting.systems.meeting.model.Meeting}s.
  */
@@ -16,28 +18,6 @@ import net.dv8tion.jda.api.entities.*;
 public class MeetingManager {
 	private final Guild guild;
 	private final Meeting meeting;
-
-	public TextChannel getLogChannel() {
-        return guild.getTextChannelById(meeting.getLogChannelId());
-	}
-
-	public VoiceChannel getVoiceChannel() {
-		return guild.getVoiceChannelById(meeting.getVoiceChannelId());
-	}
-
-	public void addParticipant(User user) {
-		var meetingLocale = LocalizationUtils.getLocale(Language.valueOf(meeting.getLanguage())).getMeeting().getLog();
-		var channel = this.getLogChannel();
-		channel.sendMessage(String.format(meetingLocale.getLOG_PARTICIPANT_ADDED(), user.getAsMention())).queue();
-		channel.getManager().putMemberPermissionOverride(user.getIdLong(), Permission.ALL_CHANNEL_PERMISSIONS, 0).queue();
-	}
-
-	public void removeParticipant(User user) {
-		var meetingLocale = LocalizationUtils.getLocale(Language.valueOf(meeting.getLanguage())).getMeeting().getLog();
-		var channel = this.getLogChannel();
-		channel.sendMessage(String.format(meetingLocale.getLOG_PARTICIPANT_REMOVED(), user.getAsMention())).queue();
-		channel.getManager().putMemberPermissionOverride(user.getIdLong(), 0, Permission.ALL_CHANNEL_PERMISSIONS).queue();
-	}
 
 	public static MessageEmbed buildMeetingCreationEmbed(int step, LocaleConfig.MeetingConfig.MeetingCreationConfig config, String description) {
 		return new EmbedBuilder()
@@ -57,6 +37,37 @@ public class MeetingManager {
 				.setFooter(locale.getMeeting().getMEETING_EMBED_FOOTER())
 				.setTimestamp(meeting.getDueAt().toLocalDateTime())
 				.build();
+	}
+
+	public TextChannel getLogChannel() {
+		return guild.getTextChannelById(meeting.getLogChannelId());
+	}
+
+	public VoiceChannel getVoiceChannel() {
+		return guild.getVoiceChannelById(meeting.getVoiceChannelId());
+	}
+
+	public void addParticipant(User user) {
+		var meetingLocale = LocalizationUtils.getLocale(Language.valueOf(meeting.getLanguage())).getMeeting().getLog();
+		var text = this.getLogChannel();
+		text.sendMessage(String.format(meetingLocale.getLOG_PARTICIPANT_ADDED(), user.getAsMention())).queue();
+		text.getManager().putMemberPermissionOverride(user.getIdLong(),
+				Permission.getRaw(Permission.VIEW_CHANNEL, Permission.MESSAGE_SEND), 0
+		).queue();
+		var voice = this.getVoiceChannel();
+		voice.getManager().putMemberPermissionOverride(user.getIdLong(),
+				Collections.singleton(Permission.VIEW_CHANNEL),
+				Collections.singleton(Permission.VOICE_CONNECT)
+		).queue();
+	}
+
+	public void removeParticipant(User user) {
+		var meetingLocale = LocalizationUtils.getLocale(Language.valueOf(meeting.getLanguage())).getMeeting().getLog();
+		var text = this.getLogChannel();
+		text.sendMessage(String.format(meetingLocale.getLOG_PARTICIPANT_REMOVED(), user.getAsMention())).queue();
+		text.getManager().putMemberPermissionOverride(user.getIdLong(), 0, Permission.ALL_PERMISSIONS).queue();
+		var voice = this.getVoiceChannel();
+		voice.getManager().putMemberPermissionOverride(user.getIdLong(), 0, Permission.ALL_PERMISSIONS).queue();
 	}
 
 }
