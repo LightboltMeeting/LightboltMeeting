a168feafc1a5898cb01f482b9b8525882bf84a5b
Dynxsty <jasonlessenich@gmail.com>
2022-02-15 18:24:12 +0100

    Cleaned up `MeetingReminderJob.java`

--- src/main/java/de/lightbolt/meeting/systems/meeting/jobs/MeetingReminderJob.java
+++ src/main/java/de/lightbolt/meeting/systems/meeting/jobs/MeetingReminderJob.java
@@ -1,51 +1,42 @@
 package de.lightbolt.meeting.systems.meeting.jobs;
 
 import de.lightbolt.meeting.Bot;
+import de.lightbolt.meeting.data.h2db.DbHelper;
+import de.lightbolt.meeting.systems.meeting.MeetingManager;
 import de.lightbolt.meeting.systems.meeting.dao.MeetingRepository;
 import de.lightbolt.meeting.systems.meeting.model.Meeting;
 import de.lightbolt.meeting.utils.localization.LocaleConfig;
-import de.lightbolt.meeting.utils.localization.LocalizationUtils;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.MessageEmbed;
-import net.dv8tion.jda.api.entities.TextChannel;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 
-import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class MeetingReminderJob implements Job {
-	private LocaleConfig locale;
-	private LocaleConfig.MeetingConfig.MeetingLogConfig logLocale;
-
 	@Override
 	public void execute(JobExecutionContext context) {
-		try {
-			String[] jobDetail = context.getJobDetail().getKey().getName().split("-");
+		String[] jobDetail = context.getJobDetail().getKey().getName().split("-");
+		DbHelper.doDaoAction(MeetingRepository::new, dao -> {
 			Optional<Meeting> meetingOptional = new MeetingRepository(Bot.dataSource.getConnection()).findById(Integer.parseInt(jobDetail[0]));
-			if (!meetingOptional.isPresent()) {
+			if (meetingOptional.isEmpty()) {
 				log.warn("Meeting doesn't exist, cannot execute reminder job.");
 				return;
 			}
 			Meeting meeting = meetingOptional.get();
-			locale = LocalizationUtils.getLocale(meeting.getLanguage());
-			logLocale = locale.getMeeting().getLog();
-			StringBuilder participants = new StringBuilder();
-			TextChannel logChannel = Bot.jda.getTextChannelById(meeting.getLogChannelId());
-
-			for (long participantId : meeting.getParticipants()) {
-				participants.append(Bot.jda.getUserById(participantId).getAsMention());
-			}
-			logChannel.sendMessage(participants).queue();
-			logChannel.sendMessageEmbeds(buildEmbed(jobDetail[2])).queue();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+			MeetingManager manager = new MeetingManager(Bot.jda, meeting);
+			manager.getLogChannel()
+					.sendMessageFormat(Arrays.stream(meeting.getParticipants()).mapToObj(l -> String.format("<@%s>", l)).collect(Collectors.joining(", ")))
+					.setEmbeds(buildReminderEmbed(meeting.getLocaleConfig().getMeeting().getLog(), jobDetail[2]))
+					.queue();
+		});
 	}
 
-	private MessageEmbed buildEmbed(String reminder) {
+	private MessageEmbed buildReminderEmbed(LocaleConfig.MeetingConfig.MeetingLogConfig logLocale, String reminder) {
 		String reminderTimeUnit;
 		if (Integer.parseInt(reminder) > 60) {
 			reminderTimeUnit = logLocale.getLOG_TIMEUNIT_HOURS();
@@ -53,7 +44,6 @@ public class MeetingReminderJob implements Job {
 		} else {
 			reminderTimeUnit = logLocale.getLOG_TIMEUNIT_MINUTES();
 		}
-
 		return new EmbedBuilder()
 				.setTitle(logLocale.getLOG_REMINDER_TITLE())
 				.setDescription(String.format(logLocale.getLOG_REMINDER_DESCRIPTION(), reminder, reminderTimeUnit))
