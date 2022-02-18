18ab7de707eb8d409b0d2c0ebddf21f360eb8ff4
Dynxsty <jasonlessenich@gmail.com>
2022-02-18 06:39:30 +0100

    made the meeting faq ephemeral

--- src/main/java/de/lightbolt/meeting/listener/ButtonListener.java
+++ src/main/java/de/lightbolt/meeting/listener/ButtonListener.java
@@ -14,7 +14,7 @@ public class ButtonListener extends ListenerAdapter {
 		String[] id = event.getComponentId().split(":");
 		LocaleConfig locale = LocalizationUtils.getLocale(Language.fromLocale(event.getUserLocale()));
 		switch (id[0]) {
-			case "meeting-faq" -> event.replyEmbeds(new MeetingFaqEmbed(locale).build()).queue();
+			case "meeting-faq" -> event.replyEmbeds(new MeetingFaqEmbed(locale).build()).setEphemeral(true).queue();
 		}
 	}
 }
